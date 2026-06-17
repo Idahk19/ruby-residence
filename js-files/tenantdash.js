0a16078d76e50836bdb0c30b470c4d42491000a6
Idahk19 <idahkarwitha9@gmail.com>
2026-06-17 19:19:52 +0300

    Use DOM to get Ids to display welcome message Add a script tag for javascript connection

--- js-files/tenantdash.js
+++ js-files/tenantdash.js
@@ -5,3 +5,9 @@ const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 if(!currentUser){
     window.location.href = "tenantlogin.html"
 }
+document.getElementById("welcomeUser").textContent =
+`Welcome, ${currentUser.fullname}`
+document.getElementById("welcomeUser").textContent =
+` Email: ${currentUser.email}`
+document.getElementById("welcomeUser").textContent =
+`House Number ${currentUser.housenumber}`
