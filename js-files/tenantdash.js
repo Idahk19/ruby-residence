3a3ec878c61bea85a24773b92fbf49a88d05560e
Idahk19 <idahkarwitha9@gmail.com>
2026-06-17 19:43:32 +0300

    Update Ids to match the ones on tenantdashboard.html

--- js-files/tenantdash.js
+++ js-files/tenantdash.js
@@ -1,5 +1,7 @@
 // pull data from the localstorage
-const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+const currentUser = 
+    JSON.parse(localStorage.getItem("currentUser")) ||
+    JSON.parse(sessionStorage.getItem("currentUser"));
 
 // protect the dashboard
 if(!currentUser){
@@ -7,7 +9,7 @@ if(!currentUser){
 }
 document.getElementById("welcomeUser").textContent =
 `Welcome, ${currentUser.fullname}`
-document.getElementById("welcomeUser").textContent =
+document.getElementById("userEmail").textContent =
 ` Email: ${currentUser.email}`
-document.getElementById("welcomeUser").textContent =
+document.getElementById("userHouse").textContent =
 `House Number ${currentUser.housenumber}`
