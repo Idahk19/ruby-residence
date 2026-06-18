0e69aa3cece9ac5ac974a8c49677d19203d4d8e0
Idahk19 <idahkarwitha9@gmail.com>
2026-06-18 16:14:01 +0300

    create a tenants display.js

---
+++ js-files/tenantdisplay.js
@@ -0,0 +1,26 @@
+import { UserManager } from "./appmethods.js";
+
+const tableBody = document.getElementById("tenantsTableBody");
+
+function loadTenants() {
+    const users = UserManager.getUsers();
+
+    tableBody.innerHTML = "";
+
+    users.forEach(user => {
+        const row = document.createElement("tr");
+
+        row.classList.add("border-b");
+
+        row.innerHTML = `
+            <td class="p-3">${user.fullname}</td>
+            <td class="p-3">${user.housenumber}</td>
+            <td class="p-3">${user.email}</td>
+            <td class="p-3 text-green-600 font-semibold">Active</td>
+        `;
+
+        tableBody.appendChild(row);
+    });
+}
+
+loadTenants();
