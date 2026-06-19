31d6af95bc3e8136dfd3a95b50c2c2b415253ff4
Idahk19 <idahkarwitha9@gmail.com>
2026-06-19 15:19:08 +0300

    Add a table that displays reported issues Add function that will display reported issues

--- js-files/appmethods.js
+++ js-files/appmethods.js
@@ -151,4 +151,5 @@ export class IssueManager {
         issues.push(issue);
         this.saveIssues(issues);
     }
+    
 }
--- js-files/issuesdisplay.js
+++ js-files/issuesdisplay.js
@@ -1,2 +1,38 @@
 import{Issue} from "./appmethods.js"
 import{IssueManager} from "./appmethods.js"
+
+
+const tableBody = document.getElementById("issuesTableBody");
+
+function displayIssues() {
+
+    const issues = IssueManager.getIssues();
+
+    tableBody.innerHTML = "";
+
+    if (issues.length === 0) {
+        tableBody.innerHTML = `
+            <tr>
+                <td colspan="5" class="p-3 text-center text-gray-500">
+                    No issues submitted yet
+                </td>
+            </tr>
+        `;
+        return;
+    }
+
+    issues.forEach(issue => {
+
+        tableBody.innerHTML += `
+            <tr class="border-b">
+                <td class="p-3">${issue.fullname}</td>
+                <td class="p-3">${issue.email}</td>
+                <td class="p-3">${issue.houseNumber}</td>
+                <td class="p-3">${issue.subject}</td>
+                <td class="p-3">${issue.message}</td>
+            </tr>
+        `;
+    });
+}
+
+displayIssues();
