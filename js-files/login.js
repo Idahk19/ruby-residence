7d35d793c74f1fb1fd1f29b64755ddc453ec9bbb
Idahk19 <idahkarwitha9@gmail.com>
2026-06-19 08:55:27 +0300

    Change the show password to an eye icon and add javascript for it to function

--- js-files/login.js
+++ js-files/login.js
@@ -12,15 +12,15 @@ const rememberMe = document.getElementById("rememberMe");
 console.log(showPassword);
 
 // show password 
-showPassword.addEventListener("change", function (e) {
+showPassword.addEventListener("click", function (e) {
     console.log("clicked checkbox");
 
-    if (this.checked) {
-        passwordInput.type = "text";
-        
-    } else {
-        passwordInput.type = "password";
-        
+    const isHidden = passwordInput.type === "password";
+
+    passwordInput.type = isHidden ? "text":"password";
+
+    if (confirmPasswordInput) {
+        confirmPasswordInput.type = isHidden ? "text" : "password";
     }
 });
 
